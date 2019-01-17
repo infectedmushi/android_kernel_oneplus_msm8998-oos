0bdd317d54fcf24a3b7a9486f9523f6e216f0a42
infected_ <infectedmushi@gmail.com>
2019-01-17 09:32:58 +0000

    update build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+KBUILD_OUTPUT=/mnt/Building/out
+MODULES_DIR=/mnt/Building/AnyKernel2-stock/modules
+
+cd /mnt/Building/op5t-stable-pie-beta
+export USE_CCACHE=1
+export VARIANT="OP5T-OOS-P-OB23"
+export HASH=`git rev-parse --short=8 HEAD`
+export KERNEL_ZIP="StockPlus-$VARIANT-$(date +%y%m%d)-$HASH"
+export LOCALVERSION=~`echo $KERNEL_ZIP`
+export ARCH=arm64
+export CROSS_COMPILE="/mnt/Building/gcc-arm-8.2-2019.01-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+make O=../out clean
+make O=../out mrproper
+make O=../out infected_defconfig
+ccache make -j32 O=../out  
+cd /mnt/Building/out
+find $KBUILD_OUTPUT -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+${CROSS_COMPILE}strip --strip-unneeded $MODULES_DIR/*.ko
+#find $MODULES_DIR -name '*.ko' -exec $KBUILD_OUTPUT/scripts/sign-file sha512 $KBUILD_OUTPUT/certs/signing_key.pem $KBUILD_OUTPUT/certs/signing_key.x509 {} \;
+#/mnt/Building/out/scripts/sign-file sha512 /mnt/Building/out/certs/signing_key.pem /mnt/Building/out/certs/signing_key.x509 *.ko
+## copy assets
+#cp -v *.ko /mnt/Building/AnyKernel2-stock/modules
+cd $KBUILD_OUTPUT/arch/arm64/boot
+cp -v Image.gz-dtb /mnt/Building/AnyKernel2-stock/zImage                     
+cd /mnt/Building/AnyKernel2-stock
+zip -r9 StockPlus-$VARIANT-$(date +%y%m%d)-$HASH.zip *
+mv -v StockPlus-$VARIANT-$(date +%y%m%d)-$HASH.zip /mnt/Building/Out_Zips
+echo "Done!!!"
